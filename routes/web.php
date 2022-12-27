7aa0194b9a6ce06c81900662235ccd63aee6a3e4
Kyaw-Zayar-Soe <kzayar160@gmail.com>
2022-12-27 11:25:14 +0630

    item-location

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\ItemLocationController;
+use App\Http\Controllers\Admin\ItemLocationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
