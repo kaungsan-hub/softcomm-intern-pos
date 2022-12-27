17e7d8ab004c8e68c7ee403666ad561824f312cc
kaungsan-hub <la86ks42@gmail.com>
2022-12-27 10:57:12 +0630

    test 5

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
-<<<<<<< HEAD
-use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ItemsController;
-=======
 use App\Http\Controllers\Admin\AdminController; 
 use App\Http\Controllers\Admin\SupplierController;
->>>>>>> origin/main
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,18 +15,11 @@ Route::prefix('admin')->group(function(){
     Route::get('/sample', [AdminController::class, 'sampleIndex']);
     Route::get('/sample/create-edit', [AdminController::class, 'sampleCreateEdit']);
 
-<<<<<<< HEAD
+    # item
     Route::resource('/items',ItemsController::class);
-=======
-    //Supplier
+    # Supplier
     Route::resource('/suppliers',SupplierController::class);
 
     // Route::get('/suppliers', [AdminController::class, 'index']);
     // Route::get('/suppliers/create-edit', [AdminController::class, 'create-edit']);
-
-
-
-    
-
->>>>>>> origin/main
 });
