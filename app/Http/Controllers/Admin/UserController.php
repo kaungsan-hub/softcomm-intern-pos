612248a7ccd4e2be214e4cabdeef14be11914bc1
unknown <ymt.yemyattun@gmail.com>
2022-12-27 10:18:48 +0630

    YMT Changed Controllers Path

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 
-use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Admin\{AdminController, AuthController, UserController};
 
 use Illuminate\Support\Facades\Route;
 
