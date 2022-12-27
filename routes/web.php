f707362d1b0aec7dd6292e24d43e9c7e51b53861
unknown <ymt.yemyattun@gmail.com>
2022-12-27 09:46:31 +0630

    YMT Auth and Users 2

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,10 @@
 <?php
 
-<<<<<<< HEAD
+
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-=======
-use App\Http\Controllers\Admin\AdminController; 
-use App\Http\Controllers\Admin\SupplierController;
->>>>>>> 68eecde95ed9b96eb6b040c14b64c5451d8deb8b
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,7 +12,6 @@ Route::get('/', function () {
 });
 
 
-<<<<<<< HEAD
 // Login
 Route::get('/login', [AuthController::class, 'showLogin']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -31,21 +27,12 @@ Route::middleware('authMiddleware')->group(function () {
 
         // User
         Route::resource('/users', UserController::class);
+
+
+        //Supplier
+        Route::resource('/suppliers', SupplierController::class);
+
+        // Route::get('/suppliers', [AdminController::class, 'index']);
+        // Route::get('/suppliers/create-edit', [AdminController::class, 'create-edit']);
     });
 });
-=======
-    Route::get('/sample', [AdminController::class, 'sampleIndex']);
-    Route::get('/sample/create-edit', [AdminController::class, 'sampleCreateEdit']);
-
-    //Supplier
-    Route::resource('/suppliers',SupplierController::class);
-
-    // Route::get('/suppliers', [AdminController::class, 'index']);
-    // Route::get('/suppliers/create-edit', [AdminController::class, 'create-edit']);
-
-
-
-    
-
-});
->>>>>>> 68eecde95ed9b96eb6b040c14b64c5451d8deb8b
